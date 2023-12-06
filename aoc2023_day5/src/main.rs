e3f5134a92c1dfea16565e7d2b9b17f0a50828a5
Nick Gruessner <kennichnicht100@gmail.com>
2023-12-06 13:11:55 +0100

    day6

--- aoc2023_day5/src/main.rs
+++ aoc2023_day5/src/main.rs
@@ -1,3 +1,5 @@
+use std::{collections::btree_map::Keys, time::Instant};
+
 use rayon::iter::{IntoParallelIterator, IntoParallelRefIterator, ParallelIterator};
 
 #[derive(Copy, Clone, Debug)]
@@ -38,14 +40,94 @@ impl Converter {
                 conversion.start_start <= number
                     && conversion.start_start + conversion.length > number
             })
-            .map_or_else(|| number, |conversion| conversion.convert(number))
+            .map_or_else(|| number, |conversion|  conversion.convert(number))
+    }
+
+    pub fn sort_conversions(&mut self) {
+        self.conversions
+            .sort_by(|a, b| a.start_start.cmp(&b.start_start));
+    }
+
+    pub fn generate_missing_conversions(&mut self) {
+        // find smallest start
+        // sort by start, then take first
+        self.sort_conversions();
+        // generate from 0 to start -1 if 0 isnt listed
+
+        let first = self.conversions[0];
+
+        let mut end_elem = first.start_start + first.length - 1;
+        if first.start_start != 0 {
+            self.conversions.push(Conversion {
+                start_start: 0,
+                destination_start: 0,
+                length: first.start_start,
+            });
+
+            self.sort_conversions();
+        }
+
+        loop {
+            // go to end and look for n+1
+            match self
+                .conversions
+                .iter()
+                .find(|x| x.start_start == end_elem + 1)
+            {
+                Some(elem) => {
+                    // if found, go to end, repeat there
+                    end_elem = elem.start_start + elem.length - 1;
+                    continue;
+                }
+                None => (),
+            }
+
+            // if not found, find smallest one bigger than current
+            // are ordered, so just take first that is bigger
+            let smallest = self
+                .conversions
+                .iter()
+                .filter(|x| x.start_start > end_elem)
+                .next();
+
+            match smallest {
+                Some(elem) => {
+                    // we clopy it, so that we can modify the list the conversion is in
+                    let elem = elem.clone();
+                    // if found, generate range up to that -1
+                    self.conversions.push(Conversion {
+                        start_start: end_elem + 1,
+                        destination_start: end_elem + 1,
+                        length: elem.start_start - (end_elem + 1),
+                    });
+
+                    self.sort_conversions();
+
+                    end_elem = elem.start_start + elem.length;
+                }
+                None => {
+                    // if not found, generate range up to isize::max and break
+                    self.conversions.push(Conversion {
+                        start_start: end_elem + 1,
+                        destination_start: end_elem + 1,
+                        length: isize::MAX - (end_elem + 1),
+                    });
+                    break;
+                }
+            }
+        }
     }
 }
 
 fn main() {
     let s = std::fs::read_to_string("input").unwrap();
-    part_1(&s);
+    //  part_1(&s);
+    /*let mut start = Instant::now();
     part_2(&s);
+    let mut end = Instant::now();
+    println!("Time: {}", (end - start).as_secs_f32());*/
+    part_2(&s);
+    part_2_2(&s);
 }
 
 fn part_1(s: &str) {
@@ -114,6 +196,9 @@ fn part_2(s: &str) {
                 let mut result = seed;
                 for converter in &converters {
                     result = converter.convert(result);
+                    if result == 136096660 {
+                        println!("Last converter: {:?}, seed: {}", converter, seed)
+                    }
                 }
                 result
             })
@@ -123,6 +208,111 @@ fn part_2(s: &str) {
     }
 
     println!("Result Part 2: {}", results.iter().min().unwrap());
+}
+
+fn part_2_2(s: &str) {
+    let mut lines = s.lines();
+
+    let seed_list = lines.next().unwrap();
+    let seed_list: Vec<isize> = seed_list
+        .split(':')
+        .nth(1)
+        .unwrap()
+        .trim()
+        .split(' ')
+        .map(|x| str::parse::<isize>(x).unwrap())
+        .collect();
+
+    let mut seed_pairs = Vec::new();
+
+    let mut first = -1;
+    for seed in &seed_list {
+        if first == -1 {
+            first = *seed;
+        } else {
+            seed_pairs.push((first, *seed));
+
+            first = -1;
+        }
+    }
+
+    let mut converters = parse_converters(lines);
+
+    for converter in &mut converters {
+        converter.generate_missing_conversions();
+    }
+
+    //println!("{:#?}", converters[0]);
+
+    let mut min = isize::MAX;
+    for (start, len) in seed_pairs {
+        let res = convert_recursive(start, start + len, &converters, vec![]);
+        min = min.min(res);
+    }
+
+    println!("Result part 2_2: {min}");
+}
+
+fn convert_recursive(range_start: isize, range_end: isize, converters: &[Converter], used_convs: Vec<(isize, isize, Conversion)>) -> isize {
+    
+    let mut conversions_min_result = isize::MAX;
+
+    for conversion in &converters[0].conversions {
+        let conversion_end = conversion.start_start + conversion.length;
+
+        // first: check out if we actually hit the conversion
+        // start of conversion is between start and end
+        if conversion.start_start >= range_start && conversion.start_start < range_end
+
+        // or end of conversion is between start and end
+        || conversion_end >= range_start && conversion_end <= range_end
+
+        // or both start and end of conversion are between start and start + len
+
+        || conversion.start_start >= range_start && conversion_end <= range_end
+        // or both or both start and end of conversion are encompassing
+        || conversion.start_start <= range_start && conversion_end >= range_end
+        {
+            // second: find out where exactly we hit it
+            // the actual start of converting with this conversion
+            let actual_start = isize::max(range_start, conversion.start_start);
+            // convert that
+            let start_of_range_conversion_result = conversion.convert(actual_start);
+
+            // convert the end of the range
+            let converted_end = conversion.convert(conversion_end.min(range_end));
+            
+            let mut used_convs = used_convs.clone();
+        
+            used_convs.push((range_start, range_end, conversion.to_owned()));
+
+            if converters.len() > 1 {
+                // now convert with all further converters
+                let result = convert_recursive(
+                    start_of_range_conversion_result,
+                    converted_end,
+                    &converters[1..],
+                    used_convs
+                );
+                // and update our minimum found result if necessary
+                conversions_min_result = conversions_min_result.min(result);
+            } else {
+                if start_of_range_conversion_result == 92661120 {
+                    for (start, end ,conv) in &used_convs {
+                        if *end < conv.start_start 
+                        || *start > conv.start_start + conv.length
+                     {
+                        println!("Somethings up");
+                        }
+                    }
+                    println!("{:#?}", used_convs);
+                }
+                return start_of_range_conversion_result;
+            }
+        }
+    }
+
+    return conversions_min_result;
 }
 
 fn parse_converters(lines: std::str::Lines<'_>) -> Vec<Converter> {
---
+++ aoc2023_day6/src/main.rs
@@ -0,0 +1,78 @@
+use std::time::Instant;
+
+use rayon::iter::{IntoParallelIterator, ParallelIterator};
+
+fn main() {
+    let input = std::fs::read_to_string("input").unwrap();
+    let mut lines = input.lines();
+    let time_line = lines.next().unwrap();
+    let distance_line = lines.next().unwrap();
+
+    part_1(time_line, distance_line);
+    let start = Instant::now();
+    part_2(time_line, distance_line);
+    let end = Instant::now();
+    println!("Time for 2: {}", (end - start).as_secs_f32());
+}
+
+fn part_1(times: &str, distances: &str) {
+    let times = parse_line(times);
+    let distances: Vec<u64> = parse_line(distances);
+
+    let mut pairs = Vec::new();
+    for i in 0..times.len() {
+        pairs.push((times[i], distances[i]));
+    }
+
+    let res = pairs
+        .iter()
+        .map(|x| determine_ways_to_win(*x))
+        .reduce(|acc, elem| acc * elem)
+        .unwrap();
+
+    println!("Result 2: {res}");
+}
+
+fn part_2(times: &str, distances: &str) {
+    let time = parse_line_2(times);
+    let distance = parse_line_2(distances);
+
+    let res = determine_ways_to_win((time, distance));
+
+    println!("Result: {res}");
+}
+
+fn determine_ways_to_win(pair: (u64, u64)) -> u64 {
+    let time = pair.0;
+    let distance = pair.1;
+
+    let sum: u64 = (1..time)
+        .into_iter()
+        .map(|charge_time_speed| {
+            let my_distance = (time - charge_time_speed) * charge_time_speed;
+            if my_distance > distance {
+                1
+            } else {
+                0
+            }
+        })
+        .sum();
+
+    sum
+}
+
+fn parse_line(line: &str) -> Vec<u64> {
+    line.split(':')
+        .nth(1)
+        .unwrap()
+        .trim()
+        .split(' ')
+        .filter(|x| !x.is_empty())
+        .map(|x| str::parse::<u64>(x).unwrap())
+        .collect()
+}
+
+fn parse_line_2(line: &str) -> u64 {
+    let number = line.split(':').nth(1).unwrap().trim().replace(" ", "");
+    str::parse::<u64>(&number).unwrap()
+}
